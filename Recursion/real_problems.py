a79939cacb0a6d74f624802cee06e09884fb9664
Singham <rahul.singh4800@gmail.com>
2019-09-18 01:02:49 +0530

    Recursion | Added New Efun function

--- Recursion/real_problems.py
+++ Recursion/real_problems.py
@@ -5,17 +5,28 @@
 """
 
 def power(num, pwr):
-    return (num ** pwr)
+    if pwr == 0:
+        return 1
+    else:
+        return num * power(num, pwr-1)
 
 
 def factorial(num):
     if (num == 0):
         return 1
     else:
-        res = num * (num - 1)
+        res = num * factorial(num - 1)
         return res
 
+def efun(num):
+    if num == 0:
+        return 1
+    else:
+        print(num * efun(num-2))
+        return num * efun(num-2)
 
 print("{} to the power of {} is {}".format(2,4, power(2,4)))
 print("{}! is {}".format(4, factorial(4)))
 print("{}! is {}".format(0, factorial(0)))
+print("-------------SPECIAL ENTRY------------------")
+print("efun: {} is {}".format(8, efun(8)))
