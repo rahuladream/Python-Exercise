a8d77c5e69c6a9763e8ce2198e1026501a871e80
Rahul Singh <rahul.singh4800@gmail.com>
2020-07-28 14:22:02 +0530

    inheriting example

--- PythonOOPs/Inheritance/inheritance2.py
+++ PythonOOPs/Inheritance/inheritance2.py
@@ -3,21 +3,30 @@ class Person:
         self.name = name
         self.age = age
     
-    def print(self):
-        print("Name: {} Age: {}".format(self.name, self.age))
+    def tell(self):
+        print("Name: {} Age:{}".format(self.name, self.age))
+    
 
-
-class Employee(Person):
-    def __init__(self, name, age, position, salary):
-        Person.__init__(self, name, age)
+class Employee:
+    def __init__(self, position, salary):
         self.position = position
         self.salary = salary
-    
-    def print(self):
-        Person.print(self)
-        print("Salary: {} Position: {}".format(self.salary, self.position))
+
+    def tell(self):
+        print("Position: {} Salary: {}".format(self.position, self.salary))
+ 
 
 class Enterprenaur(Person, Employee):
-   pass
+    def __init__(self, name, age, salary, position, exp):
+        self.exp = exp
+        Person.__init__(self, name, age)
+        Employee.__init__(self, position, salary)
+    
+    def print(self):
+        Person.tell(self)
+        Employee.tell(self)
+        print("Experience {}".format(self.exp))
 
-
+if __name__ == "__main__":
+    obj = Enterprenaur("Rahul", 30, 133410, 'HR', 10)
+    obj.print()
